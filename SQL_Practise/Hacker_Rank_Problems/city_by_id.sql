55cb5903544258cb7aae9b7c7f452494a6aba889
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2026-04-10 21:55:54 +0500

    Fetch all columns by ID
    solved the problem where i had to fetch all the columns of a specific city from the CITY table by filtering it with ID = 1661 using WHERE clause in SQL.

---
+++ SQL_Practise/Hacker_Rank_Problems/city_by_id.sql
@@ -0,0 +1,7 @@
+-- Question: We have a CITY table that has columns like ID, NAME, COUNTRYCODE,
+-- DISTRICT, and POPULATION. We need to get all the columns of that one
+-- specific city whose ID is 1661.
+
+SELECT *
+FROM CITY
+WHERE ID=1661;
