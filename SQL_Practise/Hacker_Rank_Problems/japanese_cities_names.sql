9c2514eaf6cc05bd1086abfde44ba18eb9778b36
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2026-04-10 22:19:29 +0500

    Fetch city names through Country Code
    Solved the problem where i had to fetch only the names of all japanese cities from the CITY table by filtering it with Country Code = 'JPN' using WHERE clause in SQL

---
+++ SQL_Practise/Hacker_Rank_Problems/japanese_cities_names.sql
@@ -0,0 +1,7 @@
+-- Question: We have a CITY table that has columns like ID, NAME, COUNTRYCODE,
+-- DISTRICT, and POPULATION. We need to get only the NAME of every city
+-- that belongs to Japan where the CountryCode is 'JPN'.
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE="JPN";
