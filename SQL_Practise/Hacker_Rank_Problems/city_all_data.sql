a987e732c204011904005fa51665a4b883276b40
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2026-04-10 21:54:36 +0500

    Fetch all columns
    Solved the problem where i had to fetch all the columns and all the rows from the CITY table using simple "SELECT * statement in SQL"

---
+++ SQL_Practise/Hacker_Rank_Problems/city_all_data.sql
@@ -0,0 +1,6 @@
+-- Question: We have a CITY table that has columns like ID, NAME, COUNTRYCODE,
+-- DISTRICT, and POPULATION. We need to get all the columns and every
+-- row that exists in this CITY table without any filter or condition.
+
+SELECT *
+FROM CITY;
