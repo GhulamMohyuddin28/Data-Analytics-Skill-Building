48cc4fbac9e7658358bbf9822c2f5f41f9541d27
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2026-04-10 22:21:14 +0500

    Fetch city and state
    Solved the problem where i had to fetch only the city and state columns from the STATION table using simple SELECT statement in SQL

---
+++ SQL_Practise/Hacker_Rank_Problems/station_city_state.sql
@@ -0,0 +1,6 @@
+-- Question: We have a STATION table that has columns like ID, CITY, STATE,
+-- LAT_N (northern latitude), and LONG_W (western longitude). We need to
+-- get only the CITY and STATE columns from this STATION table.
+
+SELECT CITY, STATE
+FROM STATION;
