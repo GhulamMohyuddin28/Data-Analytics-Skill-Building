5678a0f47afeae70874b5bd87695fe89048f995a
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2026-04-12 21:25:51 +0500

    fetch city names that both start and end with a vowel

---
+++ SQL_Practise/Hacker_Rank_Problems/station_cities_start_end_with_vowels.sql
@@ -0,0 +1,9 @@
+-- Question: We have a STATION table that has columns like ID, CITY, STATE,
+-- LAT_N (northern latitude), and LONG_W (western longitude). We need to
+-- get all the CITY names that both start and end with a vowel (a, e, i, o, u)
+-- and also make sure there are no duplicate city names in the result.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) IN ('a','e','i','o','u') 
+AND RIGHT(CITY,1) IN ('a','e','i','o','u');
