36f09c2e3e1264eb382317b2e6e7b662690b7a4f
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2026-04-12 21:27:08 +0500

    Fetch city names that not starting with vowel

---
+++ SQL_Practise/Hacker_Rank_Problems/station_cities_not_starting_with_vowels.sql
@@ -0,0 +1,14 @@
+-- Question: We have a STATION table that has columns like ID, CITY, STATE,
+-- LAT_N (northern latitude), and LONG_W (western longitude). We need to
+-- get all the CITY names that do not start with a vowel (a, e, i, o, u)
+-- and also make sure there are no duplicate city names in the result.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE !(
+    CITY LIKE 'a%'
+OR CITY LIKE 'e%'
+OR CITY LIKE 'i%'
+OR CITY LIKE 'o%'
+OR CITY LIKE 'u%'
+);
