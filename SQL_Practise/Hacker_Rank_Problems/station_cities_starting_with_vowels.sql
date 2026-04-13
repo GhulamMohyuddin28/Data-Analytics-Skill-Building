7485a6ad462b5bad346e147aac89ef16a2176f0d
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2026-04-13 22:08:31 +0500

    Typo

--- SQL_Practise/Hacker_Rank_Problems/station_cities_starting_with_vowels.sql
+++ SQL_Practise/Hacker_Rank_Problems/station_cities_starting_with_vowels.sql
@@ -2,3 +2,11 @@
 -- LAT_N (northern latitude), and LONG_W (western longitude). We need to
 -- get all the CITY names that start with a vowel (a, e, i, o, u) and
 -- also make sure there are no duplicate city names in the result.
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE 'a%' 
+   OR CITY LIKE 'e%' 
+   OR CITY LIKE 'i%' 
+   OR CITY LIKE 'o%' 
+   OR CITY LIKE 'u%';
