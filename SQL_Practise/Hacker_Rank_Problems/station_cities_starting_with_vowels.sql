3064cb4b3bdeecb312fa3c9956eb5a5c84682ac9
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2026-04-13 22:07:32 +0500

    Vowels starting city
    used SELECT DISTINCT on CITY and applied WHERE condition
    using LIKE operator for each vowel (a%, e%, i%, o%, u%)
    and connected them all with OR operator in SQL

---
+++ SQL_Practise/Hacker_Rank_Problems/station_cities_starting_with_vowels.sql
@@ -0,0 +1,4 @@
+-- Question: We have a STATION table that has columns like ID, CITY, STATE,
+-- LAT_N (northern latitude), and LONG_W (western longitude). We need to
+-- get all the CITY names that start with a vowel (a, e, i, o, u) and
+-- also make sure there are no duplicate city names in the result.
