3319bdf876c16224e69754f4cb142dbd09435373
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2026-04-13 22:17:30 +0500

    Add SQL query for alphabetical employee names
    Solved the problem where i had to fetch all employee names
    from the Employee table and i used ORDER BY name ASC
    to sort them in alphabetical order in SQL

---
+++ SQL_Practise/Hacker_Rank_Problems/employee_names_alphabetical_order.sql
@@ -0,0 +1,7 @@
+-- Question: We have an Employee table that has columns like employee_id, name,
+-- months, and salary. We need to get all the employee names and print them
+-- in alphabetical order.
+
+SELECT name
+FROM Employee
+ORDER BY name ASC;
