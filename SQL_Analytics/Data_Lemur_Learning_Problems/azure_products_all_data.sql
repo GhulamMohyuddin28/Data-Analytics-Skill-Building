3d9ff7c976e1c582f88caec5c3aada08d3fee1af
Ghulam Mohyuddin <ghulammohyuddin.coder@gmail.com>
2026-04-20 23:45:00 +0500

    Fetch all columns
    Solved the problem where I had to fetch all the data from
    all the columns of the products table and i used simple
    SELECT * to get every row and column in SQL

---
+++ SQL_Analytics/Data_Lemur_Learning_Problems/azure_products_all_data.sql
@@ -0,0 +1,7 @@
+-- Question: We have a products table that has columns like product_id,
+-- product_category, and product_name which contains data about different
+-- Microsoft Azure cloud products. We need to get all the data from
+-- all the columns of this products table.
+
+SELECT *
+FROM products;
